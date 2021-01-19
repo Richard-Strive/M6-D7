fb5dea83e19b1a84c3158ddebf9bbefb88ec02a7
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-19 20:03:39 +0100

    Something

--- magicScript/tableCreator.js
+++ magicScript/tableCreator.js
@@ -1,3 +1,30 @@
+//LO SCRIPT QUI PRESENTE CI PERMETTE DI CREARE ED INVIARE LE TABLES A PS
+
 require("dotenv").config;
 
 const fs = require("fs");
+
+const path = require("path");
+
+const db = require("../src/weapons/db/index");
+
+const { promisify } = require("util");
+
+const read = promisify(fs.readFile);
+
+const creatrTable = async () => {
+  try {
+    const dataPath = path.join(__dirname, "../sqlSchemas/schemas.sql");
+    const data = await read(dataPath);
+    const sqlQueryString = data.toString();
+    await db.query(sqlQueryString);
+    console.log("THE TABLES ARE CREATED!!! WELL DONE RICHARD");
+  } catch (error) {
+    console.log(
+      "OPS SOMETHING WENT WRONG. UNFORTUNATLY NOTHIG HAS BEEN CREATED!!!"
+    );
+
+    console.log(error);
+  }
+  dp.pool.end();
+};
--- src/weapons/db/index.js
+++ src/weapons/db/index.js
@@ -1,5 +1,6 @@
 // configuriamo il nostro db//
 
+const { query } = require("express");
 const { Pool } = require("pg");
 
 //creamo un enviroment con pool
@@ -7,5 +8,16 @@ const { Pool } = require("pg");
 const pool = new Pool();
 
 module.exports = {
-  // il modo per poter fare le query su postgress. Grazie a pool
+  // il modo per poter fare le query/le ricerche su postgress. Grazie a pool.
+
+  async query(text, params) {
+    const start = Data.new();
+    const res = await pool.query(text, params);
+    const duration = Data.now() - start;
+    console.info("Query executed in", duration, "ms");
+
+    return res;
+  },
+
+  pool,
 };
